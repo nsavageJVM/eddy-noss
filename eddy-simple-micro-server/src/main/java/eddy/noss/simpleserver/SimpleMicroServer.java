aba5c5ef85775cdf60641e0ca6da515010534684
nsavagejvm <nigeljvm@yahoo.com>
2016-10-24 01:08:29 +0300

    distributed code

---
+++ eddy-simple-micro-server/src/main/java/eddy/noss/simpleserver/SimpleMicroServer.java
@@ -0,0 +1,35 @@
+package eddy.noss.simpleserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ubu on 24.10.16.
+ */
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class SimpleMicroServer {
+
+    public static void main(String[] args) {
+        SpringApplication simpleMicroServer = new SpringApplication(SimpleMicroServer.class);
+        simpleMicroServer.addListeners(new ApplicationPidFileWriter("SimpleMicroServer.pid"));
+        simpleMicroServer.run(args);
+    }
+
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "This is a trivial service that demonstrates how a Eureka Client\n " +
+                "can register with a Eureka Server and discover services";
+    }
+
+
+}
--- eddy-simple-micro/src/main/java/eddy/noss/simplemicro/SimpleMicro.java
+++ eddy-simple-micro/src/main/java/eddy/noss/simplemicro/SimpleMicro.java
@@ -1,17 +1,57 @@
 package eddy.noss.simplemicro;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient //or @EnableDiscoveryClient
+@RestController
+@EnableEurekaClient
 public class SimpleMicro {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleMicro.class);
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    RestTemplate restTemplate = new RestTemplate();
+
 
     public static void main(String[] args) {
         SpringApplication notificationMicroService = new SpringApplication(SimpleMicro.class);
         notificationMicroService.addListeners(new ApplicationPidFileWriter("SimpleMicro.pid"));
         notificationMicroService.run(args);
     }
+
+
+    @RequestMapping("/")
+    String home() {
+        InstanceInfo instance = discoveryClient.getNextServerFromEureka("SMICROSERVER", false);
+        String homePageUrl = instance.getHomePageUrl();
+
+        log.info("Got homepage url {}", homePageUrl);
+
+        ResponseEntity<String> response = restTemplate.getForEntity(homePageUrl, String.class);
+
+
+        return "SimpleMicroServer notification " + response.getBody() ;
+    }
+
+    @RequestMapping("/debug")
+    String debug() {
+
+        return "SimpleMicroServer notification debug"  ;
+    }
+
+
 }
